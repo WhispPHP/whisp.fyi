e209c10847874b1ae05bd9d63da99dbf99268ad3
Ashley Hindle <hey@ashleyhindle.com>
2025-04-03 13:08:32 +0100

    remove print_rs of env/argv1 from howdy-[name]

--- apps/howdy-[name].php
+++ apps/howdy-[name].php
@@ -5,12 +5,8 @@ use function Laravel\Prompts\text;
 require __DIR__.'/vendor/autoload.php';
 
 if ($argc > 1) {
-    print_r($_ENV);
-    print_r($argv);
-    echo "Found your name through the route param!" . PHP_EOL . PHP_EOL;
     $name = $argv[1];
 } else {
-    echo "No name found, asking for your name..." . PHP_EOL . PHP_EOL;
     $name = text('What is your name?', required: true, placeholder: 'John Doe');
 }
 
