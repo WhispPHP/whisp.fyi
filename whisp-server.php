b397dcbc048549c9242e9a19d415e11585a23855
Ashley Hindle <hey@ashleyhindle.com>
2025-04-03 13:06:14 +0100

    support passing the port

--- whisp-server.php
+++ whisp-server.php
@@ -3,6 +3,6 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-(new Whisp\Server(port: 2020))
+(new Whisp\Server(port: (int) $argv[1] ?? 2020))
     ->setLogger(new Whisp\Loggers\FileLogger(__DIR__ . '/server.log'))
     ->run(); // Auto discovers apps from the 'apps' directory
