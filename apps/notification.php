997c3e00f72afe09462dc50d656d154bfe193d38
Ashley Hindle <hey@ashleyhindle.com>
2025-04-04 07:35:14 +0100

    improve notification example message

--- apps/notification.php
+++ apps/notification.php
@@ -6,7 +6,8 @@ use function Laravel\Prompts\{info, intro, outro};
 
 intro('It is notification time!');
 
-echo "\033]9;Hello notification my old friend\007";
+// Send notification to terminal for the terminal emulator to display
+echo "\033]9;Howdy from Whisp, keep being awesome!\007";
 
 info('Not all terminals support this unfortunately, but let\'s give it a bash.' . PHP_EOL);
 
