026807fdf286a82371b01dbd4160dab8a6165ad6
Ashley Hindle <hey@ashleyhindle.com>
2025-04-18 17:39:27 +0100

    update croft to 0.0.9 and periodically ping so the SSH server doesn't disconnect us

--- apps/mcp.php
+++ apps/mcp.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Croft\Server;
 
 $server = new Server('Demo MCP-over-SSH Server');
+$server->configurePing(intervalMs: 15_000); // Whisp will disconnect after 60 sec of inactivity
 $server->tool(new \Apps\Croft\Tools\CheeseTool());
 $server->tool(new \Apps\Croft\Tools\WhoWouldGiveAGreatTalkAtLaraconTool());
 $server->run();
