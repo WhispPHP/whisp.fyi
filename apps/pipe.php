3e3099e2f5c0ebebb689d72c8ea3a9dfd20c900c
Ashley Hindle <hey@ashleyhindle.com>
2025-04-19 18:23:49 +0100

    update whisp to 1.6.2 and add pipe demo

---
+++ apps/pipe.php
@@ -0,0 +1,6 @@
+<?php
+// Simply read from stdin and echo the result, but uppercase. This is for testing piping
+// i.e. echo 'howdy' | ssh pipe@whisp.fyi
+echo strtoupper(
+    fread(STDIN, 1024)
+);
